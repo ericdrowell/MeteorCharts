d19b1d78a733330d499d3242ed054cb8d02bada7
Eric Rowell <erowell83@gmail.com>
2013-10-05 11:51:31 -0700

    Number formatter now rounds to nearest whole number if value is less than 10

--- src/formatters/Number.js
+++ src/formatters/Number.js
@@ -1,8 +1,4 @@
 (function() {
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-
   MeteorCharts.Number = function(min, max, maxNumberOfLabels) {
     MeteorCharts.Formatter.call(this, min, max, maxNumberOfLabels);
     this.base = 10;
@@ -13,20 +9,23 @@
     formatShort: function(num) {
       var longestValue = this.getLongestValue();
 
-      if (longestValue < 1000) {
-        return numberWithCommas(num);
+      if (longestValue < 10) {
+        return Math.round(num);
+      }
+      else if (longestValue < 1000) {
+        return (Math.round(num * 10)/10);
       }
       // thousands
       else if (longestValue < 1000000) {
-        return numberWithCommas(Math.round(num / 1000 * 10)/10) + 'k';
+        return (Math.round(num / 1000 * 10)/10) + 'k';
       }
       // millions
       else if (longestValue < 1000000000) {
-        return numberWithCommas(Math.round(num / 1000000 * 10)/10) + 'M';
+        return (Math.round(num / 1000000 * 10)/10) + 'M';
       }
       // billions
       else {
-        return numberWithCommas(Math.round(num / 1000000000 * 10)/10) + 'B';
+        return (Math.round(num / 1000000000 * 10)/10) + 'B';
       }
     },
     start: function(num) {
